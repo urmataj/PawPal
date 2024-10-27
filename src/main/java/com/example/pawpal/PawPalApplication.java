3a3a19f47adce5b967bf60fd402885c04b714c37
Урматай <toktosunovurmataj@gmail.com>
2024-10-27 19:47:41 +0600

    Initial commit

---
+++ src/main/java/com/example/pawpal/PawPalApplication.java
@@ -0,0 +1,13 @@
+package com.example.pawpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawPalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawPalApplication.class, args);
+    }
+
+}
