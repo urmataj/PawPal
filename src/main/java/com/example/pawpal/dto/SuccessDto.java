c3b4ca1baa6a1541659b7d196ab833561846ac33
Saltanat Umarova <umarovasaltanat51@gmail.com>
2024-10-29 19:02:55 +0600

    SuccessDto class created

---
+++ src/main/java/com/example/pawpal/dto/SuccessDto.java
@@ -0,0 +1,17 @@
+package com.example.pawpal.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class SuccessDto {
+
+    boolean success;
+}
