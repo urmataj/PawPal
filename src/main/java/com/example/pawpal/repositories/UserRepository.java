da3455b548821896f905de27703a0a858cd10779
Saltanat Umarova <umarovasaltanat51@gmail.com>
2024-10-30 00:30:16 +0600

    UserRepository modified

--- src/main/java/com/example/pawpal/repositories/UserRepository.java
+++ src/main/java/com/example/pawpal/repositories/UserRepository.java
@@ -3,8 +3,8 @@ package com.example.pawpal.repositories;
 import com.example.pawpal.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    Optional<UserEntity> findByUsername(String username);
+    List<UserEntity> findAllByUsernameContainingIgnoreCase(String username);
 }
