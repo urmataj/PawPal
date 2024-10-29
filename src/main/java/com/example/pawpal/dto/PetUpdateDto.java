fed4a662748487644f10bfdffbeb9b91ab9be878
Saltanat Umarova <umarovasaltanat51@gmail.com>
2024-10-29 19:01:58 +0600

    UpdateDto class created

---
+++ src/main/java/com/example/pawpal/dto/PetUpdateDto.java
@@ -0,0 +1,24 @@
+package com.example.pawpal.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class PetUpdateDto {
+
+    private String name;
+
+    private String species;
+
+    private String breed;
+
+    private String gender;
+
+    private Integer age;
+
+    private String color;
+
+    private Integer weight;
+
+}
