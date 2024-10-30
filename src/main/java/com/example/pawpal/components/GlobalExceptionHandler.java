c78ee0340074407b47c7d68f49cb52f8d1060d74
Saltanat Umarova <umarovasaltanat51@gmail.com>
2024-10-30 08:43:47 +0600

    GlobalExceptionHandler created

---
+++ src/main/java/com/example/pawpal/components/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.example.pawpal.components;
+
+import com.example.pawpal.dto.ErrorDto;
+import com.example.pawpal.exceptions.ApiException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.nio.file.AccessDeniedException;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<ErrorDto> handleApiException(ApiException e) {
+        return ResponseEntity.status(e.getStatusCode()).body(new ErrorDto(e.getMessage(), e.getStatusCode().value()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDto> handleNotValid(MethodArgumentNotValidException e) {
+
+        String errorMessage = e
+                .getBindingResult()
+                .getFieldErrors().stream()
+                .map(ex -> ex.getField() + ":" + ex.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        return ResponseEntity.status(HttpStatus.CONFLICT.value()).body(new ErrorDto(errorMessage, HttpStatus.CONFLICT.value()));
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorDto> handleAccessDenied(AccessDeniedException e) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN.value()).body(new ErrorDto("Authentication is required", HttpStatus.FORBIDDEN.value()));
+    }
+
+}
